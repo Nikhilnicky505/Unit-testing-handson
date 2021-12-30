11648bc4afbd9ef0694b4c5783b58e772f452a9e
Nikhil <nikhilnicky505@gmail.com>
2021-12-30 13:02:10 +0000

    updates

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -2,7 +2,21 @@ import { html, fixture, expect } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
+const hi={
+  hii:"hi"
+}
+const el=await fixture(html`<basic-details .values=${hi}></basic-details>`);
+
 describe('Basic details', () => {
+  
+  it('hi',()=>{
+  
+    expect(el).to.be.accessible();
+  });
+  it('hi',()=>{
+ 
+   expect(el.values.hii).to.equals('hi');
+  });
   // Write test cases inside this block
   // refer basic-details.js files
 });
